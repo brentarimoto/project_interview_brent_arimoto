efbdc069b8d7c615b7d1eb8316f9b51f179c1b10
brentarimoto <brentarimoto@gmail.com>
2021-07-18 18:31:40 -0700

    updates for styling, and backend

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -12,34 +12,49 @@ router.post("/", async (req, res, next) => {
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
 
-    // if we already know conversation id, we can save time and just add it to message and return
-    if (conversationId) {
-      const message = await Message.create({ senderId, text, conversationId });
-      return res.json({ message, sender });
-    }
-    // if we don't have conversation id, find a conversation to make sure it doesn't already exist
+    // find a conversation between sender and recipient
     let conversation = await Conversation.findConversation(
       senderId,
       recipientId
-    );
+      );
 
+    // if conversation does not exits, make a conversation
     if (!conversation) {
+
       // create conversation
       conversation = await Conversation.create({
         user1Id: senderId,
         user2Id: recipientId,
       });
+
+      // set sender as online
       if (onlineUsers.includes(sender.id)) {
         sender.online = true;
       }
-    }
 
-    const message = await Message.create({
-      senderId,
-      text,
-      conversationId: conversation.id,
-    });
-    res.json({ message, sender });
+      // creates message
+      const message = await Message.create({
+        senderId,
+        text,
+        conversationId: conversation.id,
+      });
+
+
+      return res.json({ message, sender });
+
+    // if conversation exists, and it matches the id of the conversationId stated, then user can make message
+    } else if (conversation.id === conversationId){
+      const message = await Message.create({ senderId, text, conversationId });
+      return res.json({ message, sender });
+
+    // if conversation exists but conversation id does not match throw error
+    } else{
+      const err = new Error('ConversationId Does Not Match')
+      err.title = 'Access Denied'
+      err.errors= ['ConversationId Does Not Match'];
+      err.status= 403
+      next(err)
+    }
   } catch (error) {
     next(error);
   }
@@ -54,7 +69,6 @@ router.put("/", async (req, res, next) => {
     }
 
     const { readMessages } = req.body;
-    console.log(req.body)
 
     readMessages.forEach(async (id)=>{
       const message = await Message.findByPk(id)
