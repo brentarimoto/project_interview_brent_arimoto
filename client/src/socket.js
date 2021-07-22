e0b0a1d07e904820ba1856ffe7e4bca114c6d9c5
brentarimoto <brentarimoto@gmail.com>
2021-07-22 12:14:03 -0700

    removed unecessary read messages check, changed online users to set, added auth middleware on socket

--- client/src/socket.js
+++ client/src/socket.js
@@ -6,7 +6,7 @@ import {
 } from "./store/conversations";
 import { handleNewMessage, handleConvoRead } from './store/utils/thunkCreators'
 
-const socket = io(window.location.origin);
+const socket = io(window.location.origin,{auth: {token: localStorage.getItem("messenger-token")}});
 
 socket.on("connect", () => {
   console.log("connected to server");
